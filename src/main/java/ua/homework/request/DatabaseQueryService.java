6b43f968914d33ddcae581822cba7ed87dd35389
AndriiNez <andreynezen@gmail.com>
2023-10-02 10:33:21 +0200

    commit

--- src/main/java/ua/homework/request/DatabaseQueryService.java
+++ src/main/java/ua/homework/request/DatabaseQueryService.java
@@ -80,33 +80,4 @@ public class DatabaseQueryService {
             return new PrintProjectPrices(clientName, price);
         });
     }
-
-    public static void main(String[] args) {
-
-        List<MaxProjectCountClient> maxProjectCountClients = new DatabaseQueryService().findMaxProjectsClient();
-        for (MaxProjectCountClient client : maxProjectCountClients) {
-            System.out.println("Client: " + client.getName() + ", Max Project Count: " + client.getProjectCount());
-        }
-
-        List<MaxSalaryWorker> maxSalaryWorkers = new DatabaseQueryService().findMaxSalaryWorker();
-        for (MaxSalaryWorker worker: maxSalaryWorkers) {
-            System.out.println("name: " + worker.getName() + ", Salary:" + worker.getSalary());
-
-        }
-
-        List<LongrestProject> longrestProjects = new DatabaseQueryService().findLongrestProject();
-        for (LongrestProject project : longrestProjects) {
-            System.out.println("Client Name: " + project.getClientName() + ", Month Counts: " + project.getMonthCounts());
-        }
-
-        List<YongestEldestWorkers> yongestEldestWorkers = new DatabaseQueryService().findYongestEldestWorkers();
-        for (YongestEldestWorkers workers : yongestEldestWorkers) {
-            System.out.println("Type: " + workers.getType() + " , Name: " + workers.getName() + " , Birthday: " + workers.getBirthday());
-        }
-
-        List<PrintProjectPrices> price = new DatabaseQueryService().findProjectPrices();
-        for (PrintProjectPrices prices : price) {
-            System.out.println("Client Name: " + prices.getClientName() + " , Price: " + prices.getPrice());
-        }
-    }
 }
--- src/main/java/ua/homework/request/ResultSetMapper.java
+++ src/main/java/ua/homework/request/ResultSetMapper.java
@@ -2,6 +2,7 @@ package ua.homework.request;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 @FunctionalInterface
 public interface ResultSetMapper<T> {
     T map(ResultSet resultSet) throws SQLException;
